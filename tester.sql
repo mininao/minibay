cabee7c00530933c8a9f9c9178fd2ad0cb37fad8
mininao <maxence_aici@yahoo.fr>
2015-02-09 03:29:37 +0100

    Sign up function

--- tester.sql
+++ tester.sql
@@ -1,8 +1,52 @@
+-----------------------------
+-- Script testant toutes les procédures
+-- IMPORTANT : éxécuter init.sql avant ET après toute utilisation de ce script
+-----------------------------
+
+
 set serveroutput on;
+ 
 DECLARE
   output INTEGER;
 BEGIN
-  SELECT chabertc.check_user('francis','azerty') INTO output FROM dual;
+  dbms_output.put_line('1. Signin test: should return 0');
+  SELECT chabertc.signin('francis','azerty') INTO output FROM dual;
+  dbms_output.put_line(output);
+END;
+/
+
+DECLARE
+  output INTEGER;
+BEGIN
+  dbms_output.put_line('1. Signin test: should return 1');
+  SELECT chabertc.signin('francis','papapapa') INTO output FROM dual;
+  dbms_output.put_line(output);
+END;
+/
+
+DECLARE
+  output INTEGER;
+BEGIN
+  dbms_output.put_line('2. Signup test: should return 0');
+  SELECT chabertc.signup('tester','azertest','Gerard','Dumont','190 Rue du cheval',to_date('22/12/1902','DD/MM/YYYY'),93250,'tester@example.com','0123456789') INTO output FROM dual;
+  dbms_output.put_line(output);
+END;
+/
+
+DECLARE
+  output INTEGER;
+BEGIN
+  dbms_output.put_line('2. Signup test: should return 1');
+  SELECT chabertc.signup('tester','x','x','x','x',to_date('22/12/1902','DD/MM/YYYY'),93250,'x@x.x','x') INTO output FROM dual;
+  dbms_output.put_line(output);
+END;
+/
+
+DECLARE
+  output INTEGER;
+BEGIN
+  dbms_output.put_line('2. Signup test: should return 0');
+  SELECT chabertc.signup('tester2','x','x','x','x',to_date('22/12/1902','DD/MM/YYYY'),93250) INTO output FROM dual;
   dbms_output.put_line(output);
 END;
 /
--- user_logic.sql
+++ user_logic.sql
@@ -1,23 +1,35 @@
-CREATE OR REPLACE FUNCTION check_user(upseudo IN VARCHAR2, upass IN VARCHAR2) RETURN INTEGER AS
+CREATE OR REPLACE FUNCTION signin(upseudo IN VARCHAR2, upass IN VARCHAR2) RETURN INTEGER AS
 useless VARCHAR2(25);
 BEGIN
   SELECT pseudo INTO useless FROM USERS WHERE PSEUDO=upseudo AND PASS=upass;
-  IF SQL%NOTFOUND THEN
+  IF SQL%FOUND THEN
     RETURN 0;
-  ELSE
-    RETURN 1;
   END IF;
+  EXCEPTION
+  WHEN no_data_found THEN
+  RETURN 1;
 END;
 /
 
-CREATE OR REPLACE FUNCTION check_user(upseudo IN VARCHAR2, upass IN VARCHAR2) RETURN INTEGER AS
-useless VARCHAR2(25);
+-- signup(Pseudo,Password,FirstName,LastName,Adress,DateOfBirth,ZIP[,Email,Phone])
+-- returns 0 if successful
+-- returns 1 if pseudo already in use
+-- returns 2 if other error
+CREATE OR REPLACE FUNCTION signup(uPseudo IN VARCHAR2, uPass IN VARCHAR2, uFirst_name IN VARCHAR2, uLast_name IN VARCHAR2, uAddress IN VARCHAR2, uDate_birth IN DATE, uZip IN NUMBER, uEmail IN VARCHAR2 DEFAULT null, uPhone  IN VARCHAR2 DEFAULT null) RETURN INTEGER AS
+
+PRAGMA AUTONOMOUS_TRANSACTION;
 BEGIN
-  SELECT pseudo INTO useless FROM USERS WHERE PSEUDO=upseudo AND PASS=upass;
-  IF SQL%NOTFOUND THEN
-    RETURN 0;
-  ELSE
-    RETURN 1;
-  END IF;
+
+  INSERT INTO USERS(PSEUDO,PASS,FIRST_NAME,LAST_NAME,ADDRESS,ZIP,BALANCE,DATE_BIRTH,EMAIL,PHONE) VALUES (uPseudo,uPass,uFirst_name,uLast_name,uAddress,uZip,0,uDate_birth,uEmail,uPhone);
+  COMMIT;
+  
+  RETURN 0;
+  EXCEPTION
+  WHEN DUP_VAL_ON_INDEX THEN
+  RETURN 1;
+  WHEN OTHERS THEN
+  dbms_output.put_line(SQLCODE);
+  dbms_output.put_line(SQLERRM);
+  RETURN 2;
 END;
 /
